4a4f7f3d0223222b14c7726a402d3f9685f50159
Dmitry <schedamian@gmail.com>
2024-03-05 20:54:13 +0300

    add ziptop

--- js/trublue.js
+++ js/trublue.js
@@ -48,28 +48,33 @@ const cardsSlider3 = new Swiper(".item-model__slider-3", {
     disableOnInteraction: true, // Отключение автопрокрутки при взаимодействии пользователя с галереей
   },
   pagination: {
-    el: ".swiper-pagination-bottom-3",
+    el: ".swiper-pagination-bottom-23",
     clickable: true,
   },
 });
-const cardsSlider4 = new Swiper(".item-model__slider-4", {
-  spaceBetween: 20,
-  slidesPerView: 1,
-  loop: true,
-  fadeEffect: {
-    crossFade: true,
-  },
-  mousewheel: false,
-  speed: 800,
-  autoplay: {
-    delay: 6000, // Задержка в миллисекундах между переключениями
-    disableOnInteraction: true, // Отключение автопрокрутки при взаимодействии пользователя с галереей
-  },
-  pagination: {
-    el: ".swiper-pagination-bottom-4",
-    clickable: true,
-  },
-});
+
+const cardsSlider4Container = document.querySelector(".item-model__slider-4");
+
+if (cardsSlider4Container) {
+  const cardsSlider4 = new Swiper(".item-model__slider-4", {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true,
+    fadeEffect: {
+      crossFade: true,
+    },
+    mousewheel: false,
+    speed: 800,
+    autoplay: {
+      delay: 6000, // Задержка в миллисекундах между переключениями
+      disableOnInteraction: true, // Отключение автопрокрутки при взаимодействии пользователя с галереей
+    },
+    pagination: {
+      el: ".swiper-pagination-bottom-4",
+      clickable: true,
+    },
+  });
+}
 
 const cardsSliderBottom3 = new Swiper(".cards__bottom-3", {
   slidesPerView: 5,
