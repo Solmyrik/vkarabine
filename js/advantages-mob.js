74b79d834e9d16d43c657a1f5c4884fccc38994b
Dmitry <schedamian@gmail.com>
2024-03-11 11:19:10 +0300

    add fix

--- js/advantages-mob.js
+++ js/advantages-mob.js
@@ -4,16 +4,26 @@ if (faqItem2.length) {
     e.addEventListener("click", feqHandler);
   });
   function feqHandler(e) {
-    console.log(e.currentTarget);
+    const isActive = e.target.classList.contains("active");
+
+    faqItem2.forEach((e) => {
+      currentContent = e.parentNode.nextElementSibling;
+      currentContent.classList.remove("active");
+      currentContent.style.maxHeight = 0;
+      e.classList.remove("active");
+    });
+
     e.preventDefault();
     currentContent = e.currentTarget.parentNode.nextElementSibling;
-    console.log(currentContent);
-    e.target.classList.toggle("active");
-    currentContent.classList.toggle("active");
-    if (e.target.classList.contains("active")) {
+
+    if (!isActive) {
       currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+      e.target.classList.add("active");
+      currentContent.classList.add("active");
     } else {
       currentContent.style.maxHeight = 0;
+      e.target.classList.remove("active");
+      currentContent.classList.remove("active");
     }
   }
 }
