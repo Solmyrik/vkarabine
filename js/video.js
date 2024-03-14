bf7ec258b32eea328b5ac01afc4e0e3d67a644d5
Dmitry <schedamian@gmail.com>
2024-03-14 16:40:39 +0300

    add new pages

--- js/video.js
+++ js/video.js
@@ -1,9 +1,22 @@
 const plays = document.querySelectorAll(".device__play");
+const videos = document.querySelectorAll(".device__vid");
 
 plays.forEach((play) => {
   play.addEventListener("click", (e) => {
     const current = e.currentTarget;
     current.previousElementSibling.play();
+    current.previousElementSibling.classList.add("active");
     current.style.display = "none";
+  });
+});
+
+videos.forEach((video) => {
+  video.addEventListener("click", (e) => {
+    const current = e.currentTarget;
+    if (current.classList.contains("active")) {
+      current.pause();
+      current.classList.remove("active");
+      current.nextElementSibling.style.display = "flex";
+    }
   });
 });
